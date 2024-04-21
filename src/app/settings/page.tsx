bb4efc1ed73966bab0436460941b4c9613062a0f
Cristhofer Mendonca <cmendonca@drcontactlens.com>
2024-04-21 01:36:57 -0300

    Add changes

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,10 +1,24 @@
+import { Card } from "antd";
+
+import ContainerTitle from "../../components/common/ContainerTitle/ContainerTitle";
+import Description from "../../components/common/Description/Description";
 import Layout from "../../components/template/Layout/Layout";
 import ThemeButton from "../../components/template/ThemeButton/ThemeButton";
 
 export default function Settings() {
   return (
     <Layout subtitle="System settings" title="Settings to update system">
-      <ThemeButton />
+      <Card className=" h-min-80 mt-10">
+        <ContainerTitle title="Layout" className="mt-6 text-base">
+          <div className="flex">
+            <Description
+              label="Color Mode: "
+              className="mr-5"
+              description={<ThemeButton />}
+            />
+          </div>
+        </ContainerTitle>
+      </Card>
     </Layout>
   );
 }
