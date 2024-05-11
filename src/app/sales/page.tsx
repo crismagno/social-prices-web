739a7a7cb98c5b7917bb71b9d1a9b0a302637451
Cristhofer Mendonca <cmendonca@drcontactlens.com>
2024-05-11 01:53:04 -0300

    Add improvements

--- src/app/sales/page.tsx
+++ src/app/sales/page.tsx
@@ -1,5 +1,27 @@
+"use client";
+
+import { Button, Card } from "antd";
+
+import { PlusOutlined } from "@ant-design/icons";
+
 import Layout from "../../components/template/Layout/Layout";
 
 export default function SalesPage() {
-  return <Layout subtitle="Sales information" title="Sales"></Layout>;
+  return (
+    <Layout subtitle="Sales information" title="Sales">
+      <Card
+        title="Sales"
+        className="h-min-80 mt-5"
+        extra={
+          <>
+            <Button type="primary" onClick={() => {}} icon={<PlusOutlined />}>
+              Create Sale
+            </Button>
+          </>
+        }
+      >
+        test
+      </Card>
+    </Layout>
+  );
 }
