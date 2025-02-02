bc17cad792a2bd9ab63ef5ec873c2ad3b55b428a
Cristhofer Mendonca <cmendonca@drcontactlens.com>
2025-02-02 18:28:42 -0300

    Add changes

--- src/components/template/EmployeeProfileButton/EmployeeProfileButton.tsx
+++ src/components/template/EmployeeProfileButton/EmployeeProfileButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Tag } from "antd";
+import { Tag, Tooltip } from "antd";
 
 import useAuthData from "../../../data/context/auth/useAuthData";
 import EmployeesEnum from "../../../shared/business/employees/employees.enum";
@@ -16,13 +16,15 @@ export const EmployeeProfileButton: React.FC = () => {
   return (
     <div
       className="flex items-center py-1 pl-1 pr-5 rounded-full border shadow-sm
-      bg-gradient-to-tr from-slate-300 to bg-white text-slate-950
+      bg-gradient-to-tr from-slate-300 to bg-white text-slate-950 w-48
     "
     >
       <ImageOrDefault src={employee.avatar} />
 
       <div className="flex flex-col items-start justify-start ml-3 text-sm">
-        <label>{employee.name}</label>
+        <Tooltip title={employee.name}>
+          <label className="w-28  truncate">{employee.name}</label>
+        </Tooltip>
 
         <Tag color={EmployeesEnum.LevelColors[employee.level]}>
           {EmployeesEnum.LevelLabels[employee.level]}
