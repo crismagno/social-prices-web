0b285df7b277de328672ab2900689ed1ae53cf13
crismagno <cristhofer.mengonca@gmail.com>
2026-02-15 20:02:04 -0300

    Add changes

--- src/shared/business/products/products.type.ts
+++ src/shared/business/products/products.type.ts
@@ -1,10 +1,13 @@
-import { TTableStateSortOrder } from "../../../shared/utils/table/table-state.interface";
+import {
+  TTableStateSortOrder,
+} from '../../../shared/utils/table/table-state.interface';
 
 export interface IProductFileUploadTemplateRow {
   rowNumber: number;
   image?: string;
   name: string;
   barcode?: string;
+  sku?: string;
   description?: string;
   price?: string | number;
   quantity?: string | number;
@@ -13,6 +16,20 @@ export interface IProductFileUploadTemplateRow {
   tags?: string;
   isActive?: string;
   details?: string;
+  brand?: string;
+  releaseDate?: string;
+  expirationDate?: string;
+  colors?: string;
+  // Dimensions
+  dimensionSize?: string;
+  dimensionHeight?: string | number;
+  dimensionWidth?: string | number;
+  dimensionLength?: string | number;
+  dimensionDepth?: string | number;
+  dimensionDiameter?: string | number;
+  dimensionThickness?: string | number;
+  dimensionVolume?: string | number;
+  dimensionWeight?: string | number;
 }
 
 export interface IFiltersDownloadProducts {
