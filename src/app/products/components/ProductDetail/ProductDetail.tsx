172de72de4a7002e3456ae246b0920a040b3dcaf
crismagno <cristhofer.mengonca@gmail.com>
2026-02-17 23:27:02 -0300

    Add changes

--- src/app/products/components/ProductDetail/ProductDetail.tsx
+++ src/app/products/components/ProductDetail/ProductDetail.tsx
@@ -1,11 +1,8 @@
 "use client";
 
-import './styles.scss';
+import "./styles.scss";
 
-import {
-  useEffect,
-  useState,
-} from 'react';
+import { useEffect, useState } from "react";
 
 import {
   Button,
@@ -19,100 +16,57 @@ import {
   Upload,
   UploadFile,
   UploadProps,
-} from 'antd';
-import ImgCrop from 'antd-img-crop';
-import { RcFile } from 'antd/es/upload';
-import { isArray } from 'class-validator';
-import {
-  filter,
-  isObject,
-  map,
-} from 'lodash';
-import moment from 'moment';
-import {
-  Controller,
-  SubmitHandler,
-  useForm,
-} from 'react-hook-form';
-import z from 'zod';
+} from "antd";
+import ImgCrop from "antd-img-crop";
+import { RcFile } from "antd/es/upload";
+import { isArray } from "class-validator";
+import { filter, isObject, map } from "lodash";
+import moment from "moment";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import z from "zod";
 
-import { zodResolver } from '@hookform/resolvers/zod';
+import { zodResolver } from "@hookform/resolvers/zod";
 
-import ContainerTitle
-  from '../../../../components/common/ContainerTitle/ContainerTitle';
-import handleClientError
-  from '../../../../components/common/handleClientError/handleClientError';
-import HrCustom from '../../../../components/common/HrCustom/HrCustom';
-import LoadingFull from '../../../../components/common/LoadingFull/LoadingFull';
+import ContainerTitle from "../../../../components/common/ContainerTitle/ContainerTitle";
+import handleClientError from "../../../../components/common/handleClientError/handleClientError";
+import HrCustom from "../../../../components/common/HrCustom/HrCustom";
+import LoadingFull from "../../../../components/common/LoadingFull/LoadingFull";
 import {
   colorSchema,
   MultiColors,
-} from '../../../../components/common/MultiColors/MultiColors';
-import {
-  ProductHistoricPricesButton,
-} from '../../../../components/common/ProductHistoricPricesButton/ProductHistoricPricesButton';
-import {
-  ProductPreviousBarcodesPopover,
-} from '../../../../components/common/ProductPreviousBarcodesPopover/ProductPreviousBarcodesPopover';
-import {
-  StoreNameStatus,
-} from '../../../../components/common/StoreNameStatus/StoreNameStatus';
-import {
-  TagCategoryCustomAntd,
-} from '../../../../components/common/TagCategoryCustomAntd/TagCategoryCustomAntd';
-import {
-  TagTagCustomAntd,
-} from '../../../../components/common/TagTagCustomAntd/TagTagCustomAntd';
-import {
-  CheckboxCustomAntd,
-} from '../../../../components/custom/antd/CheckboxCustomAntd/CheckboxCustomAntd';
-import {
-  InputCustomAntd,
-} from '../../../../components/custom/antd/InputCustomAntd/InputCustomAntd';
-import {
-  InputNumberCustomAntd,
-} from '../../../../components/custom/antd/InputNumberCustomAntd/InputNumberCustomAntd';
-import {
-  SelectCustomAntd,
-} from '../../../../components/custom/antd/SelectCustomAntd/SelectCustomAntd';
-import {
-  TextareaCustomAntd,
-} from '../../../../components/custom/antd/TextareaCustomAntd/TextareaCustomAntd';
-import CreateProductDto
-  from '../../../../services/social-prices-api/products/dto/createProduct.dto';
-import UpdateProductDto
-  from '../../../../services/social-prices-api/products/dto/updateProduct.dto';
-import {
-  serviceMethodsInstance,
-} from '../../../../services/social-prices-api/service-methods';
-import CategoriesEnum
-  from '../../../../shared/business/categories/categories.enum';
-import {
-  ICategory,
-} from '../../../../shared/business/categories/categories.interface';
-import {
-  IProduct,
-} from '../../../../shared/business/products/products.interface';
-import { IStore } from '../../../../shared/business/stores/stores.interface';
-import TagsEnum from '../../../../shared/business/tags/tags.enum';
-import { ITag } from '../../../../shared/business/tags/tags.interface';
-import {
-  parseColorPickerToHexString,
-} from '../../../../shared/utils/antd/color-picker/color-picker';
-import { sortArray } from '../../../../shared/utils/array/array-functions';
-import DatesEnum from '../../../../shared/utils/dates/dates.enum';
-import { getFileUrl } from '../../../../shared/utils/images/images-helper';
-import { getImageUrl } from '../../../../shared/utils/images/images-url';
+} from "../../../../components/common/MultiColors/MultiColors";
+import { ProductHistoricPricesButton } from "../../../../components/common/ProductHistoricPricesButton/ProductHistoricPricesButton";
+import { ProductPreviousBarcodesPopover } from "../../../../components/common/ProductPreviousBarcodesPopover/ProductPreviousBarcodesPopover";
+import { StoreNameStatus } from "../../../../components/common/StoreNameStatus/StoreNameStatus";
+import { TagCategoryCustomAntd } from "../../../../components/common/TagCategoryCustomAntd/TagCategoryCustomAntd";
+import { TagTagCustomAntd } from "../../../../components/common/TagTagCustomAntd/TagTagCustomAntd";
+import { CheckboxCustomAntd } from "../../../../components/custom/antd/CheckboxCustomAntd/CheckboxCustomAntd";
+import { InputCustomAntd } from "../../../../components/custom/antd/InputCustomAntd/InputCustomAntd";
+import { InputNumberCustomAntd } from "../../../../components/custom/antd/InputNumberCustomAntd/InputNumberCustomAntd";
+import { SelectCustomAntd } from "../../../../components/custom/antd/SelectCustomAntd/SelectCustomAntd";
+import { TextareaCustomAntd } from "../../../../components/custom/antd/TextareaCustomAntd/TextareaCustomAntd";
+import CreateProductDto from "../../../../services/social-prices-api/products/dto/createProduct.dto";
+import UpdateProductDto from "../../../../services/social-prices-api/products/dto/updateProduct.dto";
+import { serviceMethodsInstance } from "../../../../services/social-prices-api/service-methods";
+import CategoriesEnum from "../../../../shared/business/categories/categories.enum";
+import { ICategory } from "../../../../shared/business/categories/categories.interface";
+import { IProduct } from "../../../../shared/business/products/products.interface";
+import { IStore } from "../../../../shared/business/stores/stores.interface";
+import TagsEnum from "../../../../shared/business/tags/tags.enum";
+import { ITag } from "../../../../shared/business/tags/tags.interface";
+import { parseColorPickerToHexString } from "../../../../shared/utils/antd/color-picker/color-picker";
+import { sortArray } from "../../../../shared/utils/array/array-functions";
+import DatesEnum from "../../../../shared/utils/dates/dates.enum";
+import { getFileUrl } from "../../../../shared/utils/images/images-helper";
+import { getImageUrl } from "../../../../shared/utils/images/images-url";
 import {
   formatterMoney,
   parserMoney,
-} from '../../../../shared/utils/strings/string';
-import {
-  useFindCategoriesByType,
-} from '../../../categories/useFindCategoriesByType';
-import { useFindStoresByUser } from '../../../stores/useFindStoresByUser';
-import { useFindTagsByType } from '../../../tags/useFindTagsByType';
-import { useFindProductById } from '../../detail/useFindProductById';
+} from "../../../../shared/utils/strings/string";
+import { useFindCategoriesByType } from "../../../categories/useFindCategoriesByType";
+import { useFindStoresByUser } from "../../../stores/useFindStoresByUser";
+import { useFindTagsByType } from "../../../tags/useFindTagsByType";
+import { useFindProductById } from "../../detail/useFindProductById";
 
 const dimensionsFormSchema = z.object({
   size: z.string().optional(),
@@ -717,8 +671,8 @@ export const ProductDetail: React.FC<Props> = ({
         </Row>
 
         <ContainerTitle title="Dimensions" className="mt-7">
-          <Row>
-            <Col xs={24} md={4} sm={6} lg={2}>
+          <Row gutter={[16, 16]}>
+            <Col xs={24} md={12} sm={12} lg={5}>
               <InputCustomAntd
                 controller={{ control, name: "dimensions.size" }}
                 label="Size"
@@ -726,7 +680,7 @@ export const ProductDetail: React.FC<Props> = ({
               />
             </Col>
 
-            <Col xs={24} md={4} sm={6} lg={2}>
+            <Col xs={24} md={12} sm={12} lg={5}>
               <InputCustomAntd
                 controller={{ control, name: "dimensions.height" }}
                 label="Height(m)"
@@ -736,7 +690,7 @@ export const ProductDetail: React.FC<Props> = ({
               />
             </Col>
 
-            <Col xs={24} md={4} sm={6} lg={2}>
+            <Col xs={24} md={12} sm={12} lg={5}>
               <InputCustomAntd
                 controller={{ control, name: "dimensions.width" }}
                 label="Width(m)"
@@ -746,7 +700,7 @@ export const ProductDetail: React.FC<Props> = ({
               />
             </Col>
 
-            <Col xs={24} md={4} sm={6} lg={2}>
+            <Col xs={24} md={12} sm={12} lg={5}>
               <InputCustomAntd
                 controller={{ control, name: "dimensions.length" }}
                 label="Length(m)"
@@ -756,7 +710,7 @@ export const ProductDetail: React.FC<Props> = ({
               />
             </Col>
 
-            <Col xs={24} md={4} sm={6} lg={2}>
+            <Col xs={24} md={12} sm={12} lg={5}>
               <InputCustomAntd
                 controller={{ control, name: "dimensions.depth" }}
                 label="Depth(m)"
@@ -766,7 +720,7 @@ export const ProductDetail: React.FC<Props> = ({
               />
             </Col>
 
-            <Col xs={24} md={4} sm={6} lg={2}>
+            <Col xs={24} md={12} sm={12} lg={5}>
               <InputCustomAntd
                 controller={{ control, name: "dimensions.diameter" }}
                 label="Diameter(m)"
@@ -776,7 +730,7 @@ export const ProductDetail: React.FC<Props> = ({
               />
             </Col>
 
-            <Col xs={24} md={4} sm={6} lg={2}>
+            <Col xs={24} md={12} sm={12} lg={5}>
               <InputCustomAntd
                 controller={{ control, name: "dimensions.thickness" }}
                 label="Thickness(m) "
@@ -786,7 +740,7 @@ export const ProductDetail: React.FC<Props> = ({
               />
             </Col>
 
-            <Col xs={24} md={4} sm={6} lg={2}>
+            <Col xs={24} md={12} sm={12} lg={5}>
               <InputCustomAntd
                 controller={{ control, name: "dimensions.volume" }}
                 label="Volume(l)"
@@ -796,7 +750,7 @@ export const ProductDetail: React.FC<Props> = ({
               />
             </Col>
 
-            <Col xs={24} md={4} sm={6} lg={2}>
+            <Col xs={24} md={12} sm={12} lg={5}>
               <InputCustomAntd
                 controller={{ control, name: "dimensions.weight" }}
                 label="Weight(kg)"
