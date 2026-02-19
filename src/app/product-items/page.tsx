7e0caf2ae9178a775dceb6675d22fc3c4fbe9ed8
crismagno <cristhofer.mengonca@gmail.com>
2026-02-19 01:19:40 -0300

    Add changes

--- src/app/product-items/page.tsx
+++ src/app/product-items/page.tsx
@@ -1,25 +1,11 @@
 "use client";
 
-import {
-  RefObject,
-  useEffect,
-  useRef,
-  useState,
-} from 'react';
+import { RefObject, useEffect, useRef, useState } from "react";
 
-import {
-  Avatar,
-  Button,
-  Card,
-  Col,
-  Image,
-  Row,
-  Tag,
-  Tooltip,
-} from 'antd';
-import moment from 'moment';
-import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context';
-import { useRouter } from 'next/navigation';
+import { Avatar, Button, Card, Col, Image, Row, Tag, Tooltip } from "antd";
+import moment from "moment";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context";
+import { useRouter } from "next/navigation";
 
 import {
   DownloadOutlined,
@@ -27,75 +13,50 @@ import {
   EnterOutlined,
   PlusOutlined,
   QuestionCircleOutlined,
+  ShoppingCartOutlined,
   UploadOutlined,
-} from '@ant-design/icons';
+} from "@ant-design/icons";
 
-import {
-  CustomRangeDatePicker,
-} from '../../components/common/CustomRangeDatePicker/CustomRangeDatePicker';
-import LoadingFull from '../../components/common/LoadingFull/LoadingFull';
-import SelectProducts
-  from '../../components/common/SelectProducts/SelectProducts';
-import {
-  TagCategoriesCustomAntd,
-} from '../../components/common/TagCategoriesCustomAntd/TagCategoriesCustomAntd';
-import {
-  TagStoresCustomAntd,
-} from '../../components/common/TagStoresCustomAntd/TagStoresCustomAntd';
-import {
-  TagTagsCustomAntd,
-} from '../../components/common/TagTagsCustomAntd/TagTagsCustomAntd';
-import {
-  UploadFilesDrawer,
-} from '../../components/common/UploadFilesDrawer/UploadFilesDrawer';
-import YesNo from '../../components/common/YesNo/YesNo';
-import TableCustomAntd2
-  from '../../components/custom/antd/TableCustomAntd2/TableCustomAntd2';
-import Layout from '../../components/template/Layout/Layout';
-import useAuthData from '../../data/context/auth/useAuthData';
-import useSocketData from '../../data/context/socket/useSocketData';
-import {
-  serviceMethodsInstance,
-} from '../../services/social-prices-api/service-methods';
-import CategoriesEnum from '../../shared/business/categories/categories.enum';
-import {
-  ICategory,
-} from '../../shared/business/categories/categories.interface';
-import FilesUploadsEnum
-  from '../../shared/business/files-uploads/files-uploads.enum';
-import ProductItemsEnum
-  from '../../shared/business/product-items/product-items.enum';
-import {
-  IProductItem,
-} from '../../shared/business/product-items/product-items.interface';
-import { IProduct } from '../../shared/business/products/products.interface';
-import SocketsEnum from '../../shared/business/sockets/sockets.enum';
-import TagsEnum from '../../shared/business/tags/tags.enum';
-import { ITag } from '../../shared/business/tags/tags.interface';
-import CommonEnum from '../../shared/common/enums/common.enum';
-import Urls from '../../shared/common/routes-app/routes-app';
-import { sortArray } from '../../shared/utils/array/array-functions';
-import DatesEnum from '../../shared/utils/dates/dates.enum';
-import { getImageUrl } from '../../shared/utils/images/images-url';
-import ImagesEnum from '../../shared/utils/images/images.enum';
-import { formatterMoney } from '../../shared/utils/strings/string';
-import { createTableState } from '../../shared/utils/table/table-state';
-import {
-  ITableStateRequest,
-} from '../../shared/utils/table/table-state.interface';
-import { useFindCategoriesByType } from '../categories/useFindCategoriesByType';
+import { CustomRangeDatePicker } from "../../components/common/CustomRangeDatePicker/CustomRangeDatePicker";
+import LoadingFull from "../../components/common/LoadingFull/LoadingFull";
+import SelectProducts from "../../components/common/SelectProducts/SelectProducts";
+import { TagCategoriesCustomAntd } from "../../components/common/TagCategoriesCustomAntd/TagCategoriesCustomAntd";
+import { TagStoresCustomAntd } from "../../components/common/TagStoresCustomAntd/TagStoresCustomAntd";
+import { TagTagsCustomAntd } from "../../components/common/TagTagsCustomAntd/TagTagsCustomAntd";
+import { UploadFilesDrawer } from "../../components/common/UploadFilesDrawer/UploadFilesDrawer";
+import YesNo from "../../components/common/YesNo/YesNo";
+import TableCustomAntd2 from "../../components/custom/antd/TableCustomAntd2/TableCustomAntd2";
+import Layout from "../../components/template/Layout/Layout";
+import useAuthData from "../../data/context/auth/useAuthData";
+import useSocketData from "../../data/context/socket/useSocketData";
+import { serviceMethodsInstance } from "../../services/social-prices-api/service-methods";
+import CategoriesEnum from "../../shared/business/categories/categories.enum";
+import { ICategory } from "../../shared/business/categories/categories.interface";
+import FilesUploadsEnum from "../../shared/business/files-uploads/files-uploads.enum";
+import ProductItemsEnum from "../../shared/business/product-items/product-items.enum";
+import { IProductItem } from "../../shared/business/product-items/product-items.interface";
+import { IProduct } from "../../shared/business/products/products.interface";
+import SocketsEnum from "../../shared/business/sockets/sockets.enum";
+import TagsEnum from "../../shared/business/tags/tags.enum";
+import { ITag } from "../../shared/business/tags/tags.interface";
+import CommonEnum from "../../shared/common/enums/common.enum";
+import Urls from "../../shared/common/routes-app/routes-app";
+import { sortArray } from "../../shared/utils/array/array-functions";
+import DatesEnum from "../../shared/utils/dates/dates.enum";
+import { getImageUrl } from "../../shared/utils/images/images-url";
+import ImagesEnum from "../../shared/utils/images/images.enum";
+import { formatterMoney } from "../../shared/utils/strings/string";
+import { createTableState } from "../../shared/utils/table/table-state";
+import { ITableStateRequest } from "../../shared/utils/table/table-state.interface";
+import { useFindCategoriesByType } from "../categories/useFindCategoriesByType";
 import {
   FilesUploadsTable,
   IFilesUploadsTableRefProps,
-} from '../files-uploads/FilesUploadsTable';
-import { useFindStoresByUser } from '../stores/useFindStoresByUser';
-import { useFindTagsByType } from '../tags/useFindTagsByType';
-import {
-  DownloadProductItemsDrawer,
-} from './components/DownloadProductItemsDrawer/DownloadProductItemsDrawer';
-import {
-  useFindProductItemsByUserTableState,
-} from './useFindProductItemsByUserTableState';
+} from "../files-uploads/FilesUploadsTable";
+import { useFindStoresByUser } from "../stores/useFindStoresByUser";
+import { useFindTagsByType } from "../tags/useFindTagsByType";
+import { DownloadProductItemsDrawer } from "./components/DownloadProductItemsDrawer/DownloadProductItemsDrawer";
+import { useFindProductItemsByUserTableState } from "./useFindProductItemsByUserTableState";
 
 export default function ProductItemsPage() {
   const { user } = useAuthData();
@@ -527,6 +488,21 @@ export default function ProductItemsPage() {
                         )
                       }
                       icon={<EnterOutlined />}
+                    />
+                  </Tooltip>
+
+                  <Tooltip title="Create Sale">
+                    <Button
+                      type="primary"
+                      onClick={() =>
+                        router.push(
+                          Urls.SALES_CREATE_BY_PRODUCT_ITEM.replace(
+                            ":productItemId",
+                            productItem._id
+                          )
+                        )
+                      }
+                      icon={<ShoppingCartOutlined />}
                     />
                   </Tooltip>
                 </Button.Group>
