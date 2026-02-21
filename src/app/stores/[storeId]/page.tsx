ba33db80746da797e9c7e0462ddfda894ec83846
crismagno <cristhofer.mengonca@gmail.com>
2026-02-21 01:07:10 -0300

    Add changes

--- src/app/stores/[storeId]/page.tsx
+++ src/app/stores/[storeId]/page.tsx
@@ -2,32 +2,36 @@
 
 import { useState } from "react";
 
-import { Button, Card, Col, Image, Modal, Row, Tabs, Tag, Tooltip } from "antd";
+import {
+  Button,
+  Card,
+  Col,
+  Descriptions,
+  Image,
+  Modal,
+  Row,
+  Tabs,
+  Tag,
+  Tooltip,
+} from "antd";
 import moment from "moment";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context";
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import { useParams, useRouter } from "next/navigation";
 
-import { BlockOutlined, EditOutlined, TagOutlined } from "@ant-design/icons";
+import { EditOutlined } from "@ant-design/icons";
 
 import Avatar from "../../../components/common/Avatar/Avatar";
 import ContainerTitle from "../../../components/common/ContainerTitle/ContainerTitle";
-import Description from "../../../components/common/Description/Description";
-import { DescriptionAddresses } from "../../../components/common/DescriptionAddresses/DescriptionAddresses";
-import { DescriptionPhoneNumbers } from "../../../components/common/DescriptionPhoneNumbers/DescriptionPhoneNumbers";
-import {
-  IconAtSymbol,
-  IconCake,
-  IconIdentification,
-  IconPencilSquare,
-  IconQuestion,
-  IconUser,
-} from "../../../components/common/icons/icons";
 import LoadingFull from "../../../components/common/LoadingFull/LoadingFull";
 import { TagCategoriesCustomAntd } from "../../../components/common/TagCategoriesCustomAntd/TagCategoriesCustomAntd";
 import { TagTagsCustomAntd } from "../../../components/common/TagTagsCustomAntd/TagTagsCustomAntd";
 import Layout from "../../../components/template/Layout/Layout";
 import CategoriesEnum from "../../../shared/business/categories/categories.enum";
+import AddressEnum from "../../../shared/business/shared/address/address.enum";
+import { IAddress } from "../../../shared/business/shared/address/address.interface";
+import PhoneNumberEnum from "../../../shared/business/shared/phone/phone-number.enum";
+import { IPhoneNumber } from "../../../shared/business/shared/phone/phone-number.interface";
 import StoresEnum from "../../../shared/business/stores/stores.enum";
 import TagsEnum from "../../../shared/business/tags/tags.enum";
 import Urls from "../../../shared/common/routes-app/routes-app";
@@ -80,17 +84,17 @@ export default function StorePage() {
             xs={24}
             sm={10}
             md={5}
-            className="flex flex-col justify-center items-center"
+            className="flex flex-col justify-start items-center"
           >
             <Avatar
               onClick={() => setPreviewOpen(true)}
               src={store.logo}
               width={240}
-              className="shadow-lg border-none cursor-pointer z-10"
-              title="See avatar"
+              className="shadow-lg border-none cursor-pointer z-10 rounded-lg mt-10"
+              title="See logo"
             />
 
-            <h3 className="md:text-2xl font-semibold text-blueGray-700 mt-1">
+            <h3 className="md:text-2xl font-semibold text-blueGray-700 mt-1 mb-1">
               {store.name}
             </h3>
 
@@ -98,13 +102,18 @@ export default function StorePage() {
               <a
                 href={`mailto:${store.email}`}
                 className="flex items-center text-sm leading-normal text-gray-400 
-                  font-bold px-2 py-1 shadow-sm rounded-lg border border-gray-300 mt-1
-                  w-min"
+                  font-bold px-2 py-1 shadow-sm rounded-lg border border-gray-300 mt-1"
               >
-                {IconAtSymbol("w-3.5 h-3.5")}
                 {store.email}
               </a>
             </Tooltip>
+
+            <Tag
+              color={StoresEnum.StatusColor[store.status]}
+              className="mt-2"
+            >
+              {StoresEnum.StatusLabel[store.status]}
+            </Tag>
           </Col>
 
           <Col xs={24} sm={14} md={19}>
@@ -122,107 +131,238 @@ export default function StorePage() {
                 </Tooltip>
               }
             >
-              <Row>
-                <Col xs={24} md={12}>
-                  <Description
-                    label="Name"
-                    description={`${store.name ?? "-"}`}
-                    leftIcon={IconUser()}
-                  />
+              <Row gutter={[16, 16]}>
+                {/* Basic Information */}
+                <Col xs={24}>
+                  <Descriptions
+                    bordered
+                    column={{ xs: 1, sm: 1, md: 2, lg: 3, xl: 3 }}
+                    size="small"
+                  >
+                    <Descriptions.Item label="Name" span={2}>
+                      <span className="font-medium">{store.name}</span>
+                    </Descriptions.Item>
 
-                  <Description
-                    label="Email"
-                    description={store.email ?? ""}
-                    leftIcon={IconAtSymbol()}
-                  />
-
-                  <Description
-                    label="Cnpj / Cpf"
-                    description={store.cnpj ?? ""}
-                    leftIcon={IconIdentification()}
-                  />
-
-                  <Description
-                    label="Started At"
-                    leftIcon={IconCake()}
-                    description={
-                      store.startedAt
-                        ? moment(store.startedAt).format(
-                            DatesEnum.Format.DDMMYYY
-                          )
-                        : "-"
-                    }
-                  />
-
-                  <Description
-                    label="Status"
-                    description={
-                      <Tag color={StoresEnum.StatusColor[store.status]}>
-                        {StoresEnum.StatusLabel[store.status]}
-                      </Tag>
-                    }
-                    leftIcon={IconQuestion()}
-                  />
-
-                  <Description
-                    label="Type"
-                    description={
-                      <Tag>
+                    <Descriptions.Item label="Type">
+                      <Tag color="blue">
                         {
                           StoresEnum.TypeLabels[
                             store.type ?? StoresEnum.Type.OTHER
                           ]
                         }
                       </Tag>
-                    }
-                    leftIcon={IconQuestion()}
-                  />
+                    </Descriptions.Item>
+
+                    <Descriptions.Item label="Email">
+                      <a href={`mailto:${store.email}`} className="text-blue-600">
+                        {store.email}
+                      </a>
+                    </Descriptions.Item>
+
+                    <Descriptions.Item label="CNPJ / CPF">
+                      <Tag color="purple">{store.cnpj || "-"}</Tag>
+                    </Descriptions.Item>
+
+                    <Descriptions.Item label="Status">
+                      <Tag color={StoresEnum.StatusColor[store.status]}>
+                        {StoresEnum.StatusLabel[store.status]}
+                      </Tag>
+                    </Descriptions.Item>
+
+                    <Descriptions.Item label="Started At">
+                      {store.startedAt
+                        ? moment(store.startedAt).format(
+                            DatesEnum.Format.DDMMYYY
+                          )
+                        : "-"}
+                    </Descriptions.Item>
+                  </Descriptions>
                 </Col>
 
-                <Col xs={24} md={12}>
-                  <Description
-                    label="Description"
-                    description={store.description}
-                    leftIcon={IconPencilSquare()}
-                  />
+                {/* Description & About */}
+                {(store.description || store.about) && (
+                  <Col xs={24}>
+                    <Descriptions bordered column={1} size="small">
+                      {store.description && (
+                        <Descriptions.Item label="Description">
+                          <div className="max-h-20 overflow-y-auto">
+                            {store.description}
+                          </div>
+                        </Descriptions.Item>
+                      )}
 
-                  <Description
-                    label="About"
-                    description={store.about}
-                    leftIcon={IconPencilSquare()}
-                  />
+                      {store.about && (
+                        <Descriptions.Item label="About">
+                          <div className="max-h-20 overflow-y-auto">
+                            {store.about}
+                          </div>
+                        </Descriptions.Item>
+                      )}
+                    </Descriptions>
+                  </Col>
+                )}
 
-                  <DescriptionPhoneNumbers phoneNumbers={store.phoneNumbers} />
+                {/* Phone Numbers */}
+                {store.phoneNumbers && store.phoneNumbers.length > 0 && (
+                  <Col xs={24}>
+                    <Descriptions bordered column={1} size="small">
+                      <Descriptions.Item label="Phone Numbers">
+                        <div className="flex flex-col gap-2">
+                          {store.phoneNumbers.map(
+                            (phone: IPhoneNumber, index: number) => (
+                              <div
+                                key={index}
+                                className="flex items-center gap-2 flex-wrap"
+                              >
+                                <Tag color="blue">
+                                  {PhoneNumberEnum.TypeLabels[phone.type]}
+                                </Tag>
+                                <a
+                                  href={`tel:${phone.number}`}
+                                  className="text-blue-600 font-medium"
+                                >
+                                  {phone.number}
+                                </a>
+                                {phone.messengers &&
+                                  phone.messengers.length > 0 && (
+                                    <div className="flex gap-1">
+                                      {phone.messengers.map(
+                                        (
+                                          messenger: PhoneNumberEnum.PhoneNumberMessenger,
+                                          messengerIndex: number
+                                        ) => (
+                                          <Tag
+                                            key={messengerIndex}
+                                            color="green"
+                                          >
+                                            {
+                                              PhoneNumberEnum
+                                                .PhoneNumberMessengerLabels[
+                                                messenger
+                                              ]
+                                            }
+                                          </Tag>
+                                        )
+                                      )}
+                                    </div>
+                                  )}
+                              </div>
+                            )
+                          )}
+                        </div>
+                      </Descriptions.Item>
+                    </Descriptions>
+                  </Col>
+                )}
 
-                  <DescriptionAddresses addresses={store.addresses} />
+                {/* Addresses */}
+                {store.addresses && store.addresses.length > 0 && (
+                  <Col xs={24}>
+                    <Descriptions bordered column={1} size="small">
+                      <Descriptions.Item label="Addresses">
+                        <div className="flex flex-col gap-3">
+                          {store.addresses.map(
+                            (address: IAddress, index: number) => (
+                              <div
+                                key={index}
+                                className="border-l-4 border-green-500 pl-3 py-2 bg-gray-50 rounded"
+                              >
+                                <div className="flex flex-wrap gap-1 mb-2">
+                                  {address.types &&
+                                    address.types.map(
+                                      (
+                                        type: AddressEnum.Type,
+                                        typeIndex: number
+                                      ) => (
+                                        <Tag key={typeIndex} color="purple">
+                                          {AddressEnum.TypesLabels[type]}
+                                        </Tag>
+                                      )
+                                    )}
+                                </div>
+                                <div className="text-sm space-y-1">
+                                  <div>
+                                    <span className="font-medium">
+                                      {address.address1}
+                                    </span>
+                                    {address.address2 && (
+                                      <span>, {address.address2}</span>
+                                    )}
+                                  </div>
+                                  <div className="text-gray-600">
+                                    {address.district}, {address.city} -{" "}
+                                    {address.state?.name}
+                                  </div>
+                                  <div className="text-gray-600">
+                                    {address.zip} - {address.country?.name}
+                                  </div>
+                                  {address.description && (
+                                    <div className="text-gray-500 italic">
+                                      {address.description}
+                                    </div>
+                                  )}
+                                </div>
+                              </div>
+                            )
+                          )}
+                        </div>
+                      </Descriptions.Item>
+                    </Descriptions>
+                  </Col>
+                )}
 
-                  <Description
-                    label="Categories"
-                    description={
-                      <div className="w-full flex">
-                        <TagCategoriesCustomAntd
-                          categories={categories}
-                          useTag
-                          categoriesIds={store.categoriesIds}
-                        />
+                {/* Categories & Tags */}
+                <Col xs={24}>
+                  <Descriptions bordered column={1} size="small">
+                    <Descriptions.Item label="Categories">
+                      <div className="max-w-full overflow-x-auto py-1">
+                        <div className="flex flex-wrap gap-1">
+                          <TagCategoriesCustomAntd
+                            categories={categories}
+                            useTag
+                            categoriesIds={store.categoriesIds}
+                          />
+                        </div>
                       </div>
-                    }
-                    leftIcon={<BlockOutlined className="text-lg" />}
-                  />
+                    </Descriptions.Item>
 
-                  <Description
-                    label="Tags"
-                    description={
-                      <div className="w-full flex">
-                        <TagTagsCustomAntd
-                          tags={tags}
-                          useTag
-                          tagsIds={store.tagsIds}
-                        />
+                    <Descriptions.Item label="Tags">
+                      <div className="max-w-full overflow-x-auto py-1">
+                        <div className="flex flex-wrap gap-1">
+                          <TagTagsCustomAntd
+                            tags={tags}
+                            useTag
+                            tagsIds={store.tagsIds}
+                          />
+                        </div>
                       </div>
-                    }
-                    leftIcon={<TagOutlined className="text-lg" />}
-                  />
+                    </Descriptions.Item>
+                  </Descriptions>
+                </Col>
+
+                {/* Dates */}
+                <Col xs={24}>
+                  <Descriptions
+                    bordered
+                    column={{ xs: 1, sm: 2, md: 2, lg: 3, xl: 3 }}
+                    size="small"
+                  >
+                    <Descriptions.Item label="Created At">
+                      {store.createdAt
+                        ? moment(store.createdAt).format(
+                            DatesEnum.Format.DDMMYYYYhhmmss
+                          )
+                        : "-"}
+                    </Descriptions.Item>
+
+                    <Descriptions.Item label="Updated At">
+                      {store.updatedAt
+                        ? moment(store.updatedAt).format(
+                            DatesEnum.Format.DDMMYYYYhhmmss
+                          )
+                        : "-"}
+                    </Descriptions.Item>
+                  </Descriptions>
                 </Col>
               </Row>
             </ContainerTitle>
