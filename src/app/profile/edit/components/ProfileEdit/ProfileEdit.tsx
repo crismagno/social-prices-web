bf2b5f55cd815b7de83ea54e67dae0bdf55d74eb
crismagno <cristhofer.mengonca@gmail.com>
2026-07-01 20:50:38 -0300

    Add changes

--- src/app/profile/edit/components/ProfileEdit/ProfileEdit.tsx
+++ src/app/profile/edit/components/ProfileEdit/ProfileEdit.tsx
@@ -1,49 +1,26 @@
 "use client";
 
-import { useMemo, useState } from 'react';
+import { useMemo, useState } from "react";
 
-import {
-  Button,
-  Col,
-  message,
-  Row,
-} from 'antd';
-import moment from 'moment';
-import {
-  SubmitHandler,
-  useForm,
-} from 'react-hook-form';
-import z from 'zod';
+import { Button, Col, message, Row, Select } from "antd";
+import moment from "moment";
+import { SubmitHandler, useForm } from "react-hook-form";
+import z from "zod";
 
-import { SaveOutlined } from '@ant-design/icons';
-import { zodResolver } from '@hookform/resolvers/zod';
+import { SaveOutlined } from "@ant-design/icons";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-import ContainerTitle
-  from '../../../../../components/common/ContainerTitle/ContainerTitle';
-import {
-  FormSelectOption,
-} from '../../../../../components/common/FormSelect/FormSelect';
-import handleClientError
-  from '../../../../../components/common/HandleClientError/HandleClientError';
-import {
-  InputCustomAntd,
-} from '../../../../../components/custom/antd/InputCustomAntd/InputCustomAntd';
-import {
-  SelectCustomAntd,
-} from '../../../../../components/custom/antd/SelectCustomAntd/SelectCustomAntd';
-import {
-  TextareaCustomAntd,
-} from '../../../../../components/custom/antd/TextareaCustomAntd/TextareaCustomAntd';
-import useAuthData from '../../../../../data/context/auth/useAuthData';
-import useLanguageData
-  from '../../../../../data/context/language/useLanguageData';
-import {
-  serviceMethodsInstance,
-} from '../../../../../services/social-prices-api/service-methods';
-import PersonEnum
-  from '../../../../../shared/business/shared/person/person.enum';
-import IUser from '../../../../../shared/business/users/user.interface';
-import DatesEnum from '../../../../../shared/utils/dates/dates.enum';
+import ContainerTitle from "../../../../../components/common/ContainerTitle/ContainerTitle";
+import handleClientError from "../../../../../components/common/HandleClientError/HandleClientError";
+import { InputCustomAntd } from "../../../../../components/custom/antd/InputCustomAntd/InputCustomAntd";
+import { SelectCustomAntd } from "../../../../../components/custom/antd/SelectCustomAntd/SelectCustomAntd";
+import { TextareaCustomAntd } from "../../../../../components/custom/antd/TextareaCustomAntd/TextareaCustomAntd";
+import useAuthData from "../../../../../data/context/auth/useAuthData";
+import useLanguageData from "../../../../../data/context/language/useLanguageData";
+import { serviceMethodsInstance } from "../../../../../services/social-prices-api/service-methods";
+import PersonEnum from "../../../../../shared/business/shared/person/person.enum";
+import IUser from "../../../../../shared/business/users/user.interface";
+import DatesEnum from "../../../../../shared/utils/dates/dates.enum";
 
 interface Props {
   className?: string;
@@ -70,7 +47,7 @@ const ProfileEdit: React.FC<Props> = ({ className = "" }) => {
         gender: z.string().nullable(),
         about: z.string().trim().nullable(),
       }),
-    [t]
+    [t],
   );
 
   const defaultValues: TFormSchema = {
@@ -160,9 +137,9 @@ const ProfileEdit: React.FC<Props> = ({ className = "" }) => {
               placeholder={t("placeholders.selectGender")}
             >
               {Object.keys(PersonEnum.Gender).map((gender: string) => (
-                <FormSelectOption key={gender} value={gender}>
+                <Select.Option key={gender} value={gender}>
                   {t(PersonEnum.GenderLabels[gender as PersonEnum.Gender])}
-                </FormSelectOption>
+                </Select.Option>
               ))}
             </SelectCustomAntd>
           </Col>
